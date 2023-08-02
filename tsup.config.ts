e142bc375b6a49a6e7d6e86c6e8c85c26113872f
zthCn <1119115587@qq.com>
2023-08-02 11:32:54 +0800

    chore: init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,10 @@
-import type { Options } from 'tsup';
+import type { Options } from 'tsup'
 
 export const tsup: Options = {
-    clean: true,
-    format: ['cjs', 'esm'],
+  entry: ['index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: true,
+  shims: false,
+  clean: true,
 }
